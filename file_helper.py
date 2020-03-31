7b52c8ffe59cac1488165109e30332cb3f72d0d6
uranian97 <uranian97@gmail.com>
2020-03-30 20:05:25 -0400

    added function to display all folders around and in a given query

--- file_helper.py
+++ file_helper.py
@@ -1,16 +1,26 @@
 import sqlite3
+import db_utils as db
 import pandas as pd
+#from pandasql import sqldf
+
+#pysqldf = lambda q: sqldf(q, globals())
 
 #gets all files withina given location
 def get_folder(folder_location, con):
+  folder_sql = f"""SELECT * 
+              FROM {db.FILELIST} 
+              WHERE Path LIKE '{folder_location}%'"""
+  
+  return pd.read_sql_query(folder_sql,con)
 
-    folder_sql = f"""SELECT file_name, kind, location 
-                FROM files 
-                WHERE location LIKE '{folder_location}%'"""
-    return pd.read_sql_query(folder_sql,con)
- #   cur.execute(folder_sql)
-   # records = cur.fetchall()
-   # return records
-  #  for row in records: 
-  #      print('name = ', row[0])
-   # cursor.close()
+def context_folders_of(files_df, con):
+  locations = files_df['Location'].drop_duplicates().values.tolist()
+  dfs = []
+  #for each unique file location get_folder
+  for l in locations:
+    df = get_folder(l, con)
+    dfs.append(df)
+  return pd.concat(dfs)
+  
+
+
