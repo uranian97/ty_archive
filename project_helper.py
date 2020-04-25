28db61bbb454d15f839b27d82a1c9ebf848f3a34
uranian97 <uranian97@gmail.com>
2020-04-25 18:46:04 -0400

    cleanup

--- project_helper.py
+++ project_helper.py
@@ -7,24 +7,23 @@ import file_helper as folders
 import re
 
 #project table name
-WEB_LIST = 'web_list'
+SITE_LIST = 'site_list'
 CHAT_LIST = 'CHAT_list'
 
-def make_project_lists():
-    print('Reading Teayoon@CHAT sheet')
+
+
+def load_chat_list():
     CHAT_path = op.abspath(op.join(os.path.dirname(__file__), f'projectlists/taeyoon_at_CHAT.xlsx'))
     CHAT_cols = ['exhibit','artist','type','title','medium','components','condition','digital','confirmed','dimension','year','ownership','location','context_or_series','images','video']
     #dataframe from Taeyoon @ Chat sheet
     chat = pd.read_excel(CHAT_path, names=CHAT_cols,usecols="A:P",skiprows=11,nrows=50, true_values=["Y"], false_values=["N"])
     chat.dropna(axis=0,how='all', inplace=True)
-
-    #chat['collab'] = pd.Series([get_collab(x) for x in chat['artist']])
+        #chat['collab'] = pd.Series([get_collab(x) for x in chat['artist']])
     #chat['artist'] = pd.Series([get_artist(x) for x in chat['artist']])
+    return chat
 
-    print(chat.to_string(index=False, columns=['exhibit','artist','type','title','medium','components','digital','year']))
-
-    print('Reading Taeyoonchoi.com sheet')
-    web_path = op.abspath(op.join(os.path.dirname(__file__), f'projectlists/taeyoonchoi_com.xlsx'))
+def load_site_list():
+    web_path = op.abspath(op.join(os.path.dirname(__file__), f'projectlists/taeyoonchoi_site.xlsx'))
     web_cols = ['project_name', 'work', 'document', 'doc_short','hierarchy','status','year','tags','collaborator','venue','finish','url','new_url']
     sheet_names = ['Urban Prog','Soft Care', 'Poetic Comp','Your Friend','Camerauto','Presence','Speakers','ISOPT']
     
@@ -34,20 +33,24 @@ def make_project_lists():
     site_list = pd.concat(list(proj_lists.values()))
     site_list.dropna(axis=0,how='all', inplace=True)
 
-    
+    return site_list
 
-
-    print(site_list.to_string(index=False, columns=['project_name', 'work', 'document', 'doc_short','year']))
-
+def make_project_lists():
+    print('Reading Teayoon@CHAT sheet')
+    chat = load_chat_list()
+    print('Reading Taeyoonchoi.com sheet')
+    site = load_site_list()
 
     chat.to_sql(CHAT_LIST, db.CON, if_exists='replace')
-    site_list.to_sql(WEB_LIST, db.CON, if_exists='replace')
+    site.to_sql(SITE_LIST, db.CON, if_exists='replace')
+    
 
     #alter + combine these lists to for the project list and the documentation list
     #get projects and artworks
         #any entry w/empty work and doc in .com
 
     #get documentations
+
 
 def get_collab(artist):
     artists = re.split(', | and ', artist)
@@ -66,18 +69,18 @@ def get_artist(artist):
 
 
 def list_projects():
-    df = pd.read_sql_query(f'SELECT project_name, work FROM {WEB_LIST}',db.CON)
+    df = pd.read_sql_query(f'SELECT project_name, work FROM {SITE_LIST}',db.CON)
     project_names = df.drop_duplicates().drop_duplicates(subset="work")
 
     return project_names
 
 def project_info(proj_name):
-    proj_sql = f'SELECT * FROM {WEB_LIST} WHERE project_name LIKE %{proj_name}%'
+    proj_sql = f'SELECT * FROM {SITE_LIST} WHERE project_name LIKE %{proj_name}%'
     df = pd.read_sql_query(proj_sql, db.CON).drop_duplicates
     return df
 
 def work_info(work_name):
-    work_sql = f'SELECT * FROM {WEB_LIST} WHERE work LIKE %{work_name}%'
+    work_sql = f'SELECT * FROM {SITE_LIST} WHERE work LIKE %{work_name}%'
     df = pd.read_sql_query(work_sql, db.CON).drop_duplicates
     return df
 
